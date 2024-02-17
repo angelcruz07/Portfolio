cf33045754d0cbbf264067aa50f8ab233562725a
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-02-17 12:39:44 -0600

    Add GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+	integrations: [tailwind()],
+	site: 'https://angelcruz07.github.io',
+	base: '/angelcruz07/angelcruz07.github.io.git'
+})
