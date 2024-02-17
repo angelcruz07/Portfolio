8f4a9e17d3075041c95559abf84b22c556bb5282
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-02-17 13:07:27 -0600

    Update base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
 	integrations: [tailwind()],
 	site: 'https://angelcruz07.github.io',
-	base: '/angelcruz07/angelcruz07.github.io.git'
+	base: '/'
 })
