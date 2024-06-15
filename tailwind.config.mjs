ef0945b7019419c27b2c2a83c33acf6057de2c4a
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-06-15 17:57:02 -0600

    feat: improvement in texts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {}
-	},
-	plugins: []
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        white: '#ffffff',
+        blueray: '#1E3A8A',
+        blue: '#2ccbf3ff',
+        violet: '#bb99fa',
+        green: '#b6d161',
+        beige: '#f5b7a4'
+      }
+    }
+  },
+  plugins: []
 }
