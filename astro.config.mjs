8e901cda6f4acedc95a1f30c14130f4d940f6bf6
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-06-16 22:07:27 -0600

    feat: add analytics vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
 
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	site: 'https://angelcruz07.github.io',
-	base: '/'
+  integrations: [tailwind()],
+  site: 'https://angelcruz07.github.io',
+  base: '/',
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 })
