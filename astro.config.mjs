b6957d717fe31905388363c9ffa0629fb16984a9
angelcruz07 <angelcruzsanchez126@gmail.com>
2025-02-02 14:21:54 -0600

    feat: update portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 
 import tailwind from '@astrojs/tailwind'
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://angelcruz07.github.io',
+  integrations: [tailwind(), icon({
+    include: {
+      mdi: ["*"],
+      ri: ['*'],
+      'simple-icons': ['*'],
+    },
+  })],
+  site: 'https://angeldev.me',
   base: '/',
   output: 'server',
   adapter: vercel({
